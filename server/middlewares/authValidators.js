0107702510843c5e3376f43c79b094e8e4647fa6
Kazeem Abolaji Kadiri <kazeem.kadiri@gmail.com>
2023-01-14 19:44:02 +0100

    add validation rules for admin data during admin registration

--- server/middlewares/authValidators.js
+++ server/middlewares/authValidators.js
@@ -1,5 +1,6 @@
 const { body } = require("express-validator");
 
+// This set of validation rules is for a casual user registration data
 const validateSignUp = [
     body("email").trim().isEmail().toLowerCase().withMessage('Invalid email'),
     body("phoneNumber").trim().isMobilePhone().withMessage('Invalid phone number'),
@@ -9,4 +10,11 @@ const validateSignUp = [
     body("password").trim().isAlphanumeric().isLength({min: 5}).withMessage("Password must be at least 5 characters")
 ]
 
-module.exports = validateSignUp;
+const validateAdminSignup = [
+    body("fullName").trim().isString().isLength({min: 5}).withMessage("Invalid Fullname"),
+    body("email").trim().isEmail().toLowerCase().withMessage('Invalid email'),
+    body("phoneNumber").trim().isMobilePhone().withMessage('Invalid phone number'),
+    body("password").trim().isAlphanumeric().isLength({min: 5}).withMessage("Password must be at least 5 characters")
+]
+
+module.exports = { validateSignUp, validateAdminSignup };
