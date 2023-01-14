212b2bac84c8295573aae4bf20580dcabf884435
Kazeem Abolaji Kadiri <kazeem.kadiri@gmail.com>
2023-01-14 18:08:50 +0100

    create schema for admin

---
+++ server/models/adminModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const adminSchema = new Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    unique: true
+  }
+});
+
+module.exports = mongoose.model("Admin", adminSchema);
