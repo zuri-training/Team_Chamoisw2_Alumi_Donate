54066bc1d9ba8789bc1c361724e3db06ecfaf7f3
Kazeem Abolaji Kadiri <kazeem.kadiri@gmail.com>
2023-01-14 21:03:02 +0100

    add methods to manage API operations of admins

--- client/src/hooks/profile.js
+++ client/src/hooks/profile.js
@@ -2,14 +2,31 @@ import useAxiosPrivate from "../api/axios"
 import { Toast } from "../pages/components/ToastAlert"
 import { SET_DONATION_LINK, UPDATE_TOKEN } from "../redux/actions"
 import { useDispatch } from 'react-redux'
+import useLoading from "./loading"
+import { useNavigate } from "react-router-dom"
+import useAuth from "./auth"
 
 const useUserProfile = () => {
     const { axiosPrivate }  = useAxiosPrivate()
     const dispatch = useDispatch()
+    const { setLoaderVisible } = useLoading()
+    const navigate = useNavigate()
+    const { tokenExpired } = useAuth()
+
+    const displayErrorMessages = error => {
+        Toast.fire({
+            icon: "error",
+            title: error.message
+        });
+    }
 
     const getUserData = async () => {
         try{
+            setLoaderVisible(true)
+
             const result = await axiosPrivate.get('/profile')
+
+            setLoaderVisible(false)
 
             if(false === result.data.data.error){
                 return result.data.data.message
@@ -17,18 +34,23 @@ const useUserProfile = () => {
                 throw new Error(result.data.data.message)
             }
         }catch(err){
-            Toast.fire({
-                icon: "error",
-                title: err.message
-            });
+            setLoaderVisible(false)
+            
+            displayErrorMessages(err)
+
+            return {}
         }
     }
 
     const updateUserData = async (formValues) => {
        
         try{
+            setLoaderVisible(true)
+
             const result = await axiosPrivate.patch("/profile/update", JSON.stringify(formValues))
            
+            setLoaderVisible(false)
+
             if(false === result.data.data.error){
                 Toast.fire({
                     icon: "success",
@@ -43,7 +65,7 @@ const useUserProfile = () => {
 
                 // The token also contains the collegeId, so a new token is generated on
                 // college institution change
-                if(result.data.data.message.updatedToken !== ''){
+                if(result.data.data.message.token !== ''){
                     dispatch({
                         type: UPDATE_TOKEN,
                         payload: result.data.data.message.token
@@ -64,25 +86,117 @@ const useUserProfile = () => {
     }
 
     const subscribeToNewsletter = async () => {
-        const response = await axiosPrivate.patch('/profile/newsletter/subscription')
+        try{
+            setLoaderVisible(true)
 
-        if(false === response.data.data.error){
+            const response = await axiosPrivate.patch('/profile/newsletter/subscription')
+
+            setLoaderVisible(false)
+
+            if(false === response.data.data.error){
+                Toast.fire({
+                    icon: "success",
+                    title: "Successfully subscribed to our newsletter"
+                })
+            }else{
+                Toast.fire({
+                    icon: "error",
+                    title: "Some errors were encountered, Please try to subscribe later"
+                })
+            }
+        }catch(err){
+            setLoaderVisible(false)
+
+            displayErrorMessages(err)
+            
+            return
+        }
+    }
+
+    const getAdmins = async () => {
+        try{
+            const response = await axiosPrivate.get('/profile/admins/all')
+
+            if(true === response.data.data.error){
+                // Unauthorized access, Admin is logged out automatically
+                if(tokenExpired(response.data.data.statusCode)){                    
+                    navigate('/admin/login')
+                    return
+                }
+
+                throw new Error(response.data.data.message)
+            }
+
+            return response.data.data.message
+        }catch(err){
+            displayErrorMessages(err)
+        }
+    }
+
+    const deleteAdmin = async (adminId) => {
+        try{
+            const response = await axiosPrivate.delete('/profile/admin/delete', {data: { adminId }})
+
+            if(true === response.data.data.error){
+                
+                // Unauthorized access, Admin is logged out automatically
+                if(tokenExpired(response.data.data.statusCode)){                    
+                    navigate('/admin/login')
+                    return
+                }
+
+                throw new Error('Failed to delete admin')
+            }
+
             Toast.fire({
                 icon: "success",
-                title: "Successfully subscribed to our newsletter"
+                title: response.data.data.message
             })
-        }else{
+
+            return true
+        }catch(err){
+            displayErrorMessages(err)
+
+            return false
+        }
+    }
+
+    const updateAdminDetails = async (formValues) => {
+        try{
+            setLoaderVisible(true)
+
+            const response = await axiosPrivate.patch('/profile/admin/update', formValues)
+
+            setLoaderVisible(false)
+
+            if(true === response.data.data.error){
+                
+                // Unauthorized access, Admin is logged out automatically
+                if(tokenExpired(response.data.data.statusCode)){                    
+                    navigate('/admin/login')
+                    return
+                }
+
+                throw new Error('Update admin details failed')
+            }
+
             Toast.fire({
-                icon: "error",
-                title: "Some errors were encountered, Please try to subscribe later"
+                icon: "success",
+                title: response.data.data.message
             })
+
+        }catch(err){    
+            displayErrorMessages(err) 
         }
     }
 
     return {
         getUserData,
         updateUserData,
-        subscribeToNewsletter
+        subscribeToNewsletter,
+        getAdmins,
+        deleteAdmin,
+        updateAdminDetails
     }
 }
 
