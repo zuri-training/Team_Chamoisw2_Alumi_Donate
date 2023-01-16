4f9f89e1c645134c75dc0d14a88f2b21387f860e
Kazeem Abolaji Kadiri <kazeem.kadiri@gmail.com>
2023-01-16 08:53:57 +0100

    add functions for deletion and update of bank details

--- client/src/hooks/banks.js
+++ client/src/hooks/banks.js
@@ -1,10 +1,14 @@
 import { Toast } from "../pages/components/ToastAlert"
 import useAxios from "./../api/axios"
 import useLoading from "./loading"
+import { useSelector } from "react-redux"
+import { useMemo } from "react"
 
 const useBanks = () => {
     const { axiosPrivate } = useAxios()
     const { setLoaderVisible } = useLoading()
+    const bankReduxData = useSelector((state) => (state.bank))
+    const bankToEdit = useMemo(() => (bankReduxData), [bankReduxData])
 
     const displayErrMsg = (err) => {
         Toast.fire({
@@ -52,10 +56,73 @@ const useBanks = () => {
 
         return response.data.data.message
     }
+
+    const deleteBank = async (bankId) => {
+        try{
+            setLoaderVisible(true)
+        
+            const response = await axiosPrivate.delete(`/banks/delete`, { data: { bankId } })
+
+            setLoaderVisible(false)
+
+            if(true === response.data.data.error){
+
+                throw new Error("Deletion failed")
+            }
+
+            Toast.fire({
+                icon: "success",
+                title: response.data.data.message
+            })
+
+            return true
+        }catch(err){
+            setLoaderVisible(false)
+            
+            displayErrMsg(err)
+
+            return false
+        }
+    }
+
+    const getBankReduxData = () => {
+        return bankToEdit
+    }
+
+    const updateBankDetails = async (formValues) => {
+        try{
+            setLoaderVisible(true)
+        
+            const response = await axiosPrivate.patch(`/banks/update`, formValues)
+
+            setLoaderVisible(false)
+
+            if(true === response.data.data.error){
+
+                throw new Error("Update failed")
+            }
+
+            Toast.fire({
+                icon: "success",
+                title: response.data.data.message
+            })
+
+            return true
+        }catch(err){
+            setLoaderVisible(false)
+            
+            displayErrMsg(err)
+
+            return false
+        }
+    }
     
     return {
         registerBank,
-        getBanks
+        getBanks,
+        deleteBank,
+        getBankReduxData,
+        updateBankDetails
     }
 }
 
